5c6c30c8afa396bcce6f0ff5a88a48f415e1626a
michael barrett <michaelbarrett343@googlemail.com>
2021-07-24 13:08:31 +0100

    watch list

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,4 +3,5 @@ class List < ApplicationRecord
     has_many :movies, through: :bookmarks ,dependent: :destroy
     validates :name, presence: true
     validates_uniqueness_of :name
+    has_one_attached :photo
 end
