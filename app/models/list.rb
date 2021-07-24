40d5825e540259e020500985d541cb1055650dda
michael barrett <michaelbarrett343@googlemail.com>
2021-07-24 15:20:41 +0100

    try again

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,7 @@
 class List < ApplicationRecord
+    has_one_attached :photo
     has_many :bookmarks
     has_many :movies, through: :bookmarks ,dependent: :destroy
     validates :name, presence: true
     validates_uniqueness_of :name
-    has_one_attached :photo
 end
